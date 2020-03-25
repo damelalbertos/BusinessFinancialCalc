dc7ef49e45f516100286c4dfc63208c12649f6d9
damelalbertos <damelalbertos@icloud.com>
2020-03-25 14:51:41 -0400

    added FinancialCalculator.java

---
+++ src/test/java/TestClass1.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import org.junit.jupiter.api.DynamicTest;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class TestClass1 {
+    @Test
+    public void test1() {
+        assertEquals(0,0);
+    }
+
+}
