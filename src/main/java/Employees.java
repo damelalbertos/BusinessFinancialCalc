e6c52aaba884b0271f2141e141b51a7dd16d89bd
thegoat <ruppatel11599@gmail.com>
2020-03-26 17:06:26 -0400

    Employee and BusinessTracker should be good to go with tests written

--- src/main/java/Employees.java
+++ src/main/java/Employees.java
@@ -1,66 +1,36 @@
-import java.text.DecimalFormat;
-import java.util.EmptyStackException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Pattern;
-
-public class Employees  {
+public class Employees {
 
     private String id;
     private double wage;
     private double hoursWorked;
 
-    private HashMap<String, Employees> employeesMap;
+    protected BusinessTracker business;
 
 
+    public Employees(BusinessTracker business){
+        this.business = business;
+    }
 
 
     public Employees(String id, double wage, double hoursWorked){
         this.id = id;
         this.wage = wage;
         this.hoursWorked = hoursWorked;
-        this.employeesMap = new HashMap<String, Employees>();
     }
 
 
 
-    /**
-     * Description: Calculates pay for employee based on their Id
-     *
-     * @param
-     */
-    public double calcPay(String id) throws IllegalArgumentException{
-
-        if(!employeesMap.containsKey(id)){
-            throw new IllegalArgumentException("Employee with id" + id + "doesn't exists");
-        } else {
-            double hours = employeesMap.get(id).hoursWorked;
-            double wage = employeesMap.get(id).wage;
-            double pay = 0;
-            if(hours <= 40){
-                pay = wage*hours;
-            }
-
-            else {
-                pay = wage*40 + calcOvertimePay(hours-40);
-            }
-            DecimalFormat newFormat = new DecimalFormat("#. ##");
-//
-//
-//        balance = Double.valueOf(newFormat.format(balance));
-//
-//        toAccount.balance = Double.valueOf(newFormat.format(toAccount.balance));
-//
-
-            return pay;
-        }
-
 
 //
+    public double getWage() throws IllegalArgumentException{
 
+        return wage;
 
 
     }
+
+
+
 
     /**
      *
@@ -68,34 +38,16 @@ public class Employees  {
      */
     public double getHoursWorked() throws IllegalArgumentException{
 
-        if(!employeesMap.containsKey(id)){
-            throw new IllegalArgumentException("Employee with id" + id + "doesn't exists");
-        }
-        return employeesMap.get(id).hoursWorked;
+
+
+        return hoursWorked;
 
 
     }
 
 
-    public void removeEmployee(String id, Employees employee){
-        employeesMap.remove(id, employee);
-    }
-
-    /**
-     *
-     *
-     */
-    public double calcOvertimePay(double hours){
-        hours = employeesMap.get(id).hoursWorked-40;
-        double wage = employeesMap.get(id).wage * 1.5;
-
-        double overtimePay = wage*hours;
-
-        return overtimePay;
 
 
-
-    }
 
 
     /**
@@ -105,7 +57,7 @@ public class Employees  {
      */
     public String getInfo() throws IllegalArgumentException{
 
-        if(!employeesMap.containsKey(id)){
+        if(!business.exists(id)){
             throw new IllegalArgumentException("Employee with id" + id + "doesn't exists");
         }
 
--- src/test/java/EmployeeTesters.java
+++ src/test/java/EmployeeTesters.java
@@ -7,14 +7,30 @@ public class EmployeeTesters {
     @Test
     public void  calcPayTest(){
 
+        BusinessTracker business = new BusinessTracker();
+
         Employees employee1 = new Employees("1000", 11.25, 36);
         Employees employee2 = new Employees("1001", 13.25, 50);
         Employees employee3 = new Employees("1002", 10, 40);
+        Employees employee4 = new Employees("1003", 15.37, 27);
+        Employees employee5 = new Employees("1004", 14.26, 42);
+        business.addAccount("1000", employee1);
+        business.addAccount("1001", employee2);
+        business.addAccount("1002", employee3);
+        business.addAccount("1003", employee4);
+        business.addAccount("1004", employee5);
 
 
-        assertEquals(405, employee1.calcPay("1000"));
-        assertEquals(738.7, employee2.calcPay("1001"));
-        assertEquals(400, employee3.calcPay("1002"));
+        assertEquals(405, business.calcPay("1000"));
+        assertEquals(728.75, business.calcPay("1001"));
+        assertEquals(400, business.calcPay("1002"));
+        assertEquals(414.99, business.calcPay("1003"));
+        assertEquals(613.18, business.calcPay("1004"));
+        assertThrows(IllegalArgumentException.class, () -> business.calcPay("134324"));
+        assertThrows(IllegalArgumentException.class, () -> business.calcPay("0"));
+
+
+
 
 
 
@@ -23,6 +39,16 @@ public class EmployeeTesters {
     @Test
     public void calcOvertimePayTest(){
 
+        BusinessTracker business = new BusinessTracker();
+
+        Employees employee = new Employees("100", 14.26, 42);
+        Employees employee2 = new Employees("1001", 13.25, 50);
+
+        business.addAccount("100", employee);
+        business.addAccount("1001", employee2);
+
+        assertEquals(42.78, business.calcOvertimePay("100", 2));
+        assertEquals(198.75, business.calcOvertimePay("1001", 10));
 
     }
 
@@ -33,6 +59,27 @@ public class EmployeeTesters {
 
 
 
+
+    }
+
+
+    @Test
+    public void getHoursWorked(){
+        BusinessTracker business = new BusinessTracker();
+        Employees employee1 = new Employees("1000", 11.25, 36);
+        Employees employee2 = new Employees("1001", 13.25, 50);
+        Employees employee3 = new Employees("1002", 10, 40);
+
+
+        business.addAccount("1000", employee1);
+        business.addAccount("1001", employee2);
+        business.addAccount("1002", employee3);
+        assertEquals(36, business.getEmployee("1000").getHoursWorked());
+        assertEquals(50, business.getEmployee("1001").getHoursWorked());
+        assertEquals(40, business.getEmployee("1002").getHoursWorked());
+
+        assertThrows(IllegalArgumentException.class, () -> business.getEmployee("12342").getHoursWorked());
+        assertThrows(IllegalArgumentException.class, () -> business.getEmployee("0").getHoursWorked());
 
     }
 
