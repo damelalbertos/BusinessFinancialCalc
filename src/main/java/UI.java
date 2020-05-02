b734c8335bb0b86716e73a8cbef8b82c42c1895f
thegoat <ruppatel11599@gmail.com>
2020-05-02 13:19:37 -0400

    began setting dates for UI and randomized orderedAmount

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -1,3 +1,7 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Random;
 import java.util.Scanner;
 
 
@@ -6,6 +10,51 @@ import java.util.Scanner;
 public class UI {
 
     public static void main(String[] args) throws ItemAlreadyExistsException {
+
+        Random rand = new Random();
+
+
+
+
+
+
+        String oldDate = "2020-01-01";
+        //System.out.println("Date before Addition: "+oldDate);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar c = Calendar.getInstance();
+        try{
+            c.setTime(sdf.parse(oldDate));
+        }catch(ParseException e){
+            e.printStackTrace();
+        }
+        //Incrementing the date by 1 day
+        c.add(Calendar.DAY_OF_MONTH, 1);
+        String newDate = sdf.format(c.getTime());
+        //System.out.println("Date Incremented by One: "+newDate);
+
+
+
+
+
+        while(newDate != "2020-12-31"){
+            int orderAmount = rand.nextInt((100-25)+1)+25;
+            int counter =0;
+
+            while(counter < orderAmount){
+
+
+
+            }
+
+
+
+
+
+
+
+        }
+
+
 
         CentralBusiness bus1 = new CentralBusiness("Business 1");
 
